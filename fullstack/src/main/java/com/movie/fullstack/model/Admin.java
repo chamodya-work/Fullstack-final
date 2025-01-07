3ad4acf57939a0ec3cddfe6fd895685dce6416b0
chamodya <chamodyarajapaksha1@gmail.com>
2025-01-07 11:14:13 +0530

    added Admin class

---
+++ fullstack/src/main/java/com/movie/fullstack/model/Admin.java
@@ -0,0 +1,24 @@
+package com.movie.fullstack.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@EqualsAndHashCode(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Document
+public class Admin extends User{
+
+    @Transient
+    public static final String SEQUENCE_NAME="admin_sequence";
+
+    @Id
+    private String admin_id;
+}
+
