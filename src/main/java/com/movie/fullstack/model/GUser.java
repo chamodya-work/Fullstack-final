bb6f7544f7d4558df10b988346ab523c3dbebdbe
SANDUN55 <sandunchinthakab55@gmail.com>
2025-03-06 09:09:15 +0530

    delete review added

---
+++ src/main/java/com/movie/fullstack/model/GUser.java
@@ -0,0 +1,24 @@
+package com.movie.fullstack.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Document
+@AllArgsConstructor
+@NoArgsConstructor
+public class GUser extends User{
+
+    @Transient
+    public static final String SEQUENCE_NAME="gUser_sequence";
+
+    @Id
+    private String gUser_id;
+}
+
---
+++ src/main/java/com/movie/fullstack/model/User.java
@@ -0,0 +1,18 @@
+package com.movie.fullstack.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document
+public class User {
+    private String name;
+    private String email;
+    private String password;
+}
+
